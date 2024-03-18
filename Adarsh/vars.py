6a0a6ae0b8016ad1ba8dbb22a49e6be76ea85636
frea45 <110092585+frea45@users.noreply.github.com>
2024-03-18 13:02:12 +0330

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -9,20 +9,20 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID', '20389440'))
-    API_HASH = str(getenv('API_HASH', 'a1a06a18eb9153e9dbd447cfd5da2457'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN', '6655526779:AAHkcPzBZDNtGYRfndozqEbocl1YqnyQVWE'))
+    API_ID = int(getenv('API_ID', '3335796'))
+    API_HASH = str(getenv('API_HASH', '138b992a0e672e8346d8439c3f42ea78')
+    BOT_TOKEN = str(getenv('BOT_TOKEN', '5088657122:AAELk-O6R8rYxzqXNvWWRhtl2O0-FNLwHS0'))
     name = str(getenv('name', 'filetolink'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002062053288'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001792962793'))
     PORT = int(getenv('PORT', 8000))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1169076058").split())  
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "763990585").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'useless07'))
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'haaaarki'))
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME'))
@@ -32,9 +32,9 @@ class Var(object):
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
-        URL = "https://simple-nat-etamilbooks.koyeb.app/".format(FQDN)
+        URL = "https://vipdlp-dlchinhub.koyeb.app/".format(FQDN)
     else:
-        URL = "https://simple-nat-etamilbooks.koyeb.app/".format(FQDN)
+        URL = "https://vipdlp-dlchinhub.koyeb.app/".format(FQDN)
     DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://sushankm16:4i1WAfPYKWyqPIDD@cluster0.sngp9pz.mongodb.net/?retryWrites=true&w=majority'))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'TamilBots'))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'ir_botz'))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
